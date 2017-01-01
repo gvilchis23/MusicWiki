e6bdd0c6323e4fda351e25ab48aff4547c6e6866
guillermo.vilchis <guillermo.vilchis@globant.com>
2017-01-01 14:23:36 -0500

    twitter login.

--- app/src/main/java/com/mac/musicwiki/MainActivity.java
+++ app/src/main/java/com/mac/musicwiki/MainActivity.java
@@ -1,13 +1,33 @@
 package com.mac.musicwiki;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
+
+import com.mac.musicwiki.login.LoginFragment;
 
 public class MainActivity extends AppCompatActivity {
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "memoPilgrim";
+    private static final String TWITTER_SECRET = "tarantino";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
+        Fragment newFragment = new LoginFragment();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        // Replace whatever is in the fragment_container view with this fragment,
+        // and add the transaction to the back stack
+        transaction.replace(R.id.loginFragment, newFragment);
+        transaction.commit();
     }
 }
