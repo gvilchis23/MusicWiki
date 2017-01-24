33fd07239e1b02ddcf3ad43d9de5642ae6b01825
guillermo.vilchis <guillermo.vilchis@globant.com>
2017-01-24 18:14:12 -0500

    share button

--- app/src/main/java/com/mac/musicwiki/album/view/AlbumActivity.java
+++ app/src/main/java/com/mac/musicwiki/album/view/AlbumActivity.java
@@ -3,11 +3,14 @@ package com.mac.musicwiki.album.view;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.mac.musicwiki.App;
@@ -63,5 +66,31 @@ public class AlbumActivity extends AppCompatActivity implements AlbumView {
     @Override
     public boolean addToFavorite(String artist) {
         return false;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_share, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) { switch(item.getItemId()) {
+        case R.id.add:
+            //add the function to perform here
+            return(true);
+        case R.id.fav:
+            //add the function to perform here
+            return(true);
+        case R.id.share:
+            share();
+            return(true);
+    }
+        return(super.onOptionsItemSelected(item));
+    }
+
+    public boolean share(){
+        return true;
     }
 }
